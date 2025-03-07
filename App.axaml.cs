d027a41b0d7ebf7451e82b66e160bf4709cd5f78
prinida <andreyprinida@gmail.com>
2025-03-07 22:36:27 +0700

    Add initial Avalonia MVVM ReactiveUI project

---
+++ App.axaml.cs
@@ -0,0 +1,30 @@
+using ApproximationByBezier.ViewModels;
+using ApproximationByBezier.Views;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+
+namespace ApproximationByBezier
+{
+    public partial class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ApproximationByBezier.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
